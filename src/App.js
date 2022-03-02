1555f790962a5227ddf5177ca96072e2cd064abd
inescsim <ines.csim@gmail.com>
2022-03-02 22:18:46 +0000

    update packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 import './App.scss'; 
 import Home from './components/Home';
@@ -7,50 +7,48 @@ import AboutMe from './components/AboutMe';
 import Content from './components/Content';
 import NotFound from './components/NotFound';
 
-function App() {
+const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <HashRouter>
-      <Switch>
-        <Route 
-          path="/" exact
-          render={(props) => (
-            <Content {...props}
-              darkTheme={darkTheme}
-              setDarkTheme={setDarkTheme}
-              background="home-background"
-              componentToDisplay={<Home/>}
-              photoCreditAuthor="Maria Stewart"
-              photoID="EKrOmnkwABg"
-            />
-          )}
-        />
-        <Route 
-          path="/about"
-          render={(props) => (
-            <Content {...props}
-              darkTheme={darkTheme}
-              setDarkTheme={setDarkTheme}
-              componentToDisplay={<AboutMe/>}
-            />
-          )}
-        />
-        <Route 
-          path="*"
-          render={(props) => (
-            <Content {...props}
-              darkTheme={darkTheme}
-              setDarkTheme={setDarkTheme}
-              background="not-found-background"
-              componentToDisplay={<NotFound/>}
-              photoCreditAuthor="Paul Hanaoka"
-              photoID="LcAZcVWsCIo"
-            />
-          )}
-        />
-      </Switch>
-    </HashRouter>
+      <Routes>
+          <Route
+              path="/"
+              element={
+                  <Content
+                      darkTheme={darkTheme}
+                      setDarkTheme={setDarkTheme}
+                      background="home-background"
+                      componentToDisplay={<Home/>}
+                      photoCreditAuthor="Maria Stewart"
+                      photoID="EKrOmnkwABg"
+                  />
+              }
+          />
+          <Route
+              path="/about"
+              element={
+                  <Content
+                      darkTheme={darkTheme}
+                      setDarkTheme={setDarkTheme}
+                      componentToDisplay={<AboutMe/>}
+                  />
+              }
+          />
+          <Route
+              path="*"
+              element={
+                  <Content
+                      darkTheme={darkTheme}
+                      setDarkTheme={setDarkTheme}
+                      background="not-found-background"
+                      componentToDisplay={<NotFound/>}
+                      photoCreditAuthor="Paul Hanaoka"
+                      photoID="LcAZcVWsCIo"
+                  />
+              }
+          />
+      </Routes>
   );
 }
 
